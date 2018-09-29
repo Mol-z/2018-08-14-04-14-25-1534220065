aeee8c8793c61fc52a6eb6cdb3fec365feed2483
zmy <zhangmengying@buaa.edu.cn>
2018-09-29 15:08:25 +0800

    4 changed main file

--- main/main.js
+++ main/main.js
@@ -2,10 +2,10 @@ module.exports = function main(str) {
 	var num_struc1 = ["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
 	var num_struc2 = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
 	var num_struc3 = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
-    var out_struc1;
-	var out_struc2;
-	var out_struc3;
-	var out_struc;
+    var out_struc1 = new Array(str.length);
+	var out_struc2 = new Array(str.length);
+	var out_struc3 = new Array(str.length);
+	var out_struc = new Array(3);
 	var num;
 	
 	for(var i = 0; i < str.length; i++)
@@ -15,15 +15,8 @@ module.exports = function main(str) {
 		out_struc2.push(num_struc2(num));
 		out_struc3.push(num_struc3(num));
 	}
-	out_struc1.join(' ');
-	out_struc2.join(' ');
-	out_struc3.join(' ');
 	
-	out_struc1.push('\n');
-	out_struc2.push('\n');
-	out_struc3.push('\n');
-	
-	out_struc = out_struc1 + out_struc2 + out_struc3;
+	out_struc = out_struc1.join(' ') +  '\n' + out_struc2.join(' ') +  '\n' + out_struc3.join(' ') +  '\n';
 	console.log(out_struc);
     return out_struc;
 };
